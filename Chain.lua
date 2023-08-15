76c93bbe754e761d1bd69f0f10749255378d5509
radectheblm <radecforever@gmail.com>
2023-08-15 09:45:54 -0600

    2.6: Settings moved to user.xml
    Skillchain combos, mobs defaults, spell selection moved to settings.
    Plans for manual builder scrapped, users can define chains in the settings xml

--- Chain.lua
+++ Chain.lua
@@ -1,7 +1,7 @@
 _addon.author = 'Radec'
 _addon.command = 'ch'
 _addon.name = 'chain'
-_addon.version = '2.5'
+_addon.version = '2.6'
 
 --Changelog
 --v1: string builder, auto SC picking
@@ -11,9 +11,7 @@ _addon.version = '2.5'
 --v2.3: toggle to fallback from helix to normal spell if helix cannot be cast
 --v2.4: Added settings file
 --v2.5: Adjust print feedback, add verbosity setting
-
---TODO
---	manual builder, ie ch wind earth wind dark for sci det grav ala ongo. Maybe skip this? could just add them to the table
+--v2.6: Skillchain combos, mobs defaults, spell selection moved to settings. Plans for manual builder scrapped, users can define chains in the settings xml
 
 require('tables')
 res = require('resources')
@@ -23,95 +21,63 @@ config = require('config')
 -- default settings
 
 defaults = {}
-defaults.default_helix = true --closes chains with helix1 to extend burst window. B/F Bosses are blocked from using this.
-defaults.allow_helix_recast_fallback = true --2.3 feature, if helix is on recast, use a t1 insteal
-defaults.announce_channel = 'party' --which channel to call out your actions in. 
-defaults.default_chain = 'Fusion' --default choice when sc is not specified, and not a known mob
-defaults.verbosity = 1 --0 = none: Only extremely necessary messages. 1 = low: error messages and settings changed messages. 2 = high: all messages and delays shown
-defaults.feedback_channel = 'console' --console: print() statements, chat: add_to_chat() statements
+defaults.IO = {}
+defaults.IO.default_helix = true --closes chains with helix1 to extend burst window. B/F Bosses are blocked from using this.
+defaults.IO.allow_helix_recast_fallback = true --2.3 feature, if helix is on recast, use a t1 insteal
+defaults.IO.announce_channel = 'party' --which channel to call out your actions in. 
+defaults.IO.default_chain = 'Fusion' --default choice when sc is not specified, and not a known mob
+defaults.IO.verbosity = 1 --0 = none: Only extremely necessary messages. 1 = low: error messages and settings changed messages. 2 = high: all messages and delays shown
+defaults.IO.feedback_channel = 'console' --console: print() statements, chat: add_to_chat() statements
 defaults.wait = {}
 defaults.wait.post_ja = 1.3
 defaults.wait.post_spell = 4.0
 defaults.wait.post_helix_opener = 7.0
 defaults.wait.post_helix = 8.0 --Works up to 9.5 for thunder-pyro-[settings.wait.post_helix]-iono, but is inconsistent. Less than 8.5 is safer
+defaults.skillchains = {}
+defaults.skillchains.Scission = {'Fire','Earth'}
+defaults.skillchains.Reverberation = {"Earth","Water"}
+defaults.skillchains.Detonation = {"Lightning","Wind"}
+defaults.skillchains.Liquefaction = {"Lightning","Fire"}
+defaults.skillchains.Induration = {"Water","Ice"}
+defaults.skillchains.Impaction = {"Ice","Lightning"}
+defaults.skillchains.Compression = {"Ice","Dark"}
+defaults.skillchains.Transfixion = {"Dark","Light"}
+defaults.skillchains.Fusion = {"Fire","Lightning"}
+defaults.skillchains.Gravitation = {"Wind","Dark"}
+defaults.skillchains.Distortion = {"Light","Earth"}
+defaults.skillchains.Fragmentation = {"Ice","Water"}
+defaults.skillchains.Liqfusion = {"Lightning","Fire","Lightning"}
+defaults.chain_by_mob = {}
+defaults.chain_by_mob['Ghatjot'] = "Liqfusion"
+defaults.chain_by_mob['BiunePorxie'] = "Liqfusion"
+defaults.chain_by_mob['CachaemicBhoot'] = "Liqfusion"
+defaults.chain_by_mob['Skomora'] = "Liqfusion"
+defaults.chain_by_mob['DemisangDeleterious'] = "Liqfusion"
+defaults.chain_by_mob['EsurientBotulus'] = "Liqfusion"
+defaults.chain_by_mob['Dhartok'] = "Liqfusion"
+defaults.chain_by_mob['FetidIxion'] = "Gravitation"
+defaults.chain_by_mob['GyvewrappedNaraka'] = "Liqfusion"
+defaults.chain_by_mob['Triboulex'] = "Liqfusion"
+defaults.chain_by_mob['HaughtyTulittia'] = "Liqfusion"
+defaults.chain_by_mob['Yggdreant'] = "Fragmentation"
+defaults.chain_by_mob['Rockfin'] = "Fragmentation"
+defaults.chain_by_mob['Bztavian'] = "Induration"
+defaults.chain_by_mob['Gabbrath'] = "Reverberation"
+defaults.chain_by_mob['Cehuetzi'] = "Fusion"
+defaults.chain_by_mob['Waktza'] = "Gravitation"
+defaults.chain_spells = {}
+defaults.chain_spells.Earth = {normal="Stone", helix="Geohelix"}
+defaults.chain_spells.Water = {normal="Water", helix="Hydrohelix"}
+defaults.chain_spells.Wind = {normal="Aero", helix="Anemohelix"}
+defaults.chain_spells.Fire = {normal="Fire", helix="Pyrohelix"}
+defaults.chain_spells.Ice = {normal="Blizzard", helix="Cryohelix"}
+defaults.chain_spells.Lightning = {normal="Thunder", helix="Ionohelix"}
+defaults.chain_spells.Dark = {normal="Noctohelix", helix="Noctohelix"}
+defaults.chain_spells.Light = {normal="Luminohelix", helix="Luminohelix"}
 
 settings = config.load(defaults)
 
 last_ws = {}
-
-spells_to_chain = T{
-	['Earth'] =		{normal="Stone",		helix="Geohelix"},
-	['Water'] =		{normal="Water",		helix="Hydrohelix"},
-	['Wind'] =		{normal="Aero",			helix="Anemohelix"},
-	['Fire'] =		{normal="Fire",			helix="Pyrohelix"},
-	['Ice'] =		{normal="Blizzard",		helix="Cryohelix"},
-	['Lightning'] =	{normal="Thunder",		helix="Ionohelix"},
-	['Dark'] =		{normal="Noctohelix",	helix="Noctohelix"},
-	['Light'] =		{normal="Luminohelix",	helix="Luminohelix"},
-}
-
---Alternate viable forms are commented out. The choice typically prevents absorbs, or avoid opening with a helix when possible
-skillchain_steps = T{
-
-	['Scission'] =      {"Fire","Earth"},
-	--['Scission'] =      {"Wind","Earth"},
-
-	['Reverberation'] = {"Earth","Water"},
-	--['Reverberation'] = {"Light","Water"},
-
-	['Detonation'] =    {"Lightning","Wind"},
-	--['Detonation'] =    {"Earth","Wind"},
-	--['Detonation'] =    {"Dark","Wind"},
-
-	['Liquefaction'] =  {"Lightning","Fire"},
-	--['Liquefaction'] =  {"Earth","Fire"}, --Equally viable, but lightning-fire are both light-side elements which could be a factor
-
-	['Induration'] =    {"Water","Ice"},
-
-	['Impaction'] =     {"Ice","Lightning"},
-	--['Impaction'] =     {"Water","Lightning"},
-
-	['Compression'] =   {"Ice","Dark"},
-	--['Compression'] =   {"Light","Dark"},
-
-	['Transfixion'] =   {"Dark","Light"},
-
-	['Fusion'] =        {"Fire","Lightning"},
-	['Gravitation'] =   {"Wind","Dark"},
-	['Distortion'] =    {"Light","Earth"},
-	['Fragmentation'] = {"Ice","Water"},
-
-	['Liqfusion'] = 	{"Lightning","Fire","Lightning"},
-	--['Liqfusion'] = 	{"Earth","Fire","Lightning"}, --Equally viable, but lightning-fire are both light-side elements which could be a factor
-}
-
-default_chains_by_mob_name = T{
-	['Ghatjot'] = "Liqfusion",
-
-	['Biune Porxie'] = "Liqfusion",
-
-	['Cachaemic Bhoot'] = "Liqfusion",
-	['Skomora'] = "Liqfusion",
-
-	['Demisang Deleterious'] = 'Liqfusion',
-
-	['Esurient Botulus'] = 'Liqfusion',
-	['Dhartok'] = "Liqfusion",
-
-	['Fetid Ixion'] = "Gravitation",
-
-	['Gyvewrapped Naraka'] = "Liqfusion",
-	['Triboulex'] = "Liqfusion",
-
-	['Haughty Tulittia'] = "Liqfusion",
-
-	['Yggdreant'] = "Fragmentation",
-	['Rockfin'] = "Fragmentation",
-	['Bztavian'] = "Induration",
-	['Gabbrath'] = "Reverberation",
-	['Cehuetzi'] = "Fusion",
-	['Waktza'] = "Gravitation",
-}
 
 windower.register_event("addon command", function (...)
     local params = {...}
@@ -123,44 +89,44 @@ windower.register_event("addon command", function (...)
 
     if #params == 1 then
 	    if params[1] == "helix" then
-	    	settings.default_helix = not settings.default_helix
+	    	settings.IO.default_helix = not settings.IO.default_helix
 	    	settings:save()
-	    	feedback("Using Helix closers: "..tostring(settings.default_helix), 0)
+	    	feedback("Using Helix closers: "..tostring(settings.IO.default_helix), 0)
 	    	return
 	    elseif params[1] == "fallback" then
-	    	settings.allow_helix_recast_fallback = not settings.allow_helix_recast_fallback
+	    	settings.IO.allow_helix_recast_fallback = not settings.IO.allow_helix_recast_fallback
 	    	settings:save()
-	    	feedback("Using alternatives when Helix unavailable: "..tostring(settings.allow_helix_recast_fallback), 0)
+	    	feedback("Using alternatives when Helix unavailable: "..tostring(settings.IO.allow_helix_recast_fallback), 0)
 	    	return
-	    elseif skillchain_steps[firstToUpper(params[1])] ~= nil then
+	    elseif settings.skillchains[firstToUpper(params[1])] ~= nil then
 	    	skillchain_name = firstToUpper(params[1])
 	    elseif params[1] == "lf" or params[1] == "3step" then
 	    	skillchain_name = "Liqfusion"
 	    else
-	    	skillchain_name = fmatch(params[1], keys(skillchain_steps))
+	    	skillchain_name = fmatch(params[1], keys(settings.skillchains))
 	    end
 	elseif #params == 2 then
 		if params[1] == "verbosity" then
 			local param_2_numeric = tonumber(params[2], 10)
 			if param_2_numeric then
 				if param_2_numeric <= 2 and param_2_numeric >= 0 then
-					settings.verbosity = param_2_numeric
+					settings.IO.verbosity = param_2_numeric
 					settings:save()
-					feedback("Verbosity set to: "..settings.verbosity, 0)
+					feedback("Verbosity set to: "..settings.IO.verbosity, 0)
 				end
 			end
 			return
 		elseif params[1] == "feedback" then
 			if T{'console', 'chat'}:contains(params[2]) then
-				settings.feedback_channel = params[2]
+				settings.IO.feedback_channel = params[2]
 				settings:save()
-				feedback("Feedback channel set to: "..settings.feedback_channel, 0)
+				feedback("Feedback channel set to: "..settings.IO.feedback_channel, 0)
 			end
 			return
 		end
 	end
 
-	make_skillchain(skillchain_name, settings.default_helix)
+	make_skillchain(skillchain_name)
 end)
 
 windower.register_event("load", function (...)
@@ -241,7 +207,7 @@ function firstToUpper(str)
     return (str:gsub("^%l", string.upper))
 end
 
-function make_skillchain(chain_name, use_helix)
+function make_skillchain(chain_name)
 	local player = windower.ffxi.get_player()
 	if player['target_index'] == nil then
 		feedback("No target", 1)
@@ -252,24 +218,26 @@ function make_skillchain(chain_name, use_helix)
 		windower.send_command("lua u chain")
 	end
 
-	local target_name = windower.ffxi.get_mob_by_index(player['target_index'])['name']
+	local target_name_no_spaces = windower.ffxi.get_mob_by_index(player['target_index'])['name']:gsub("%s+", "")
 	local buffs = T(player['buffs'])
 	local abil_recasts = T(windower.ffxi.get_ability_recasts())
 	local spell_recasts = T(windower.ffxi.get_spell_recasts())
 
 
 	--Don't MPK tanks
-	if S{'Leshonn','Gartell'}:contains(target_name) then
+	if S{'Leshonn','Gartell'}:contains(target_name_no_spaces) then
 		use_helix = false
+	else
+		use_helix = settings.IO.default_helix
 	end
 
 	if chain_name == 'auto' then
-		if default_chains_by_mob_name[target_name] ~= nil then
-			chain_name = default_chains_by_mob_name[target_name]
-		elseif S{'Leshonn','Degei','Gartell','Aita'}:contains(target_name) then
+		if settings.chain_by_mob[target_name_no_spaces] ~= nil then
+			chain_name = settings.chain_by_mob[target_name_no_spaces]
+		elseif S{'Leshonn','Degei','Gartell','Aita'}:contains(target_name_no_spaces) then
 			chain_name = bdgh_skillchain(player['target_index'])
 		else
-			chain_name = settings.default_chain
+			chain_name = settings.IO.default_chain
 		end
 	end
 
@@ -279,7 +247,7 @@ function make_skillchain(chain_name, use_helix)
 		active_immanence = 33
 	end
 
-	if skillchain_steps[chain_name] ~= nil then
+	if settings.skillchains[chain_name] ~= nil then
 		local execution_time = 0
 		local failure_reason = nil
 		local command_list = T{}
@@ -296,7 +264,7 @@ function make_skillchain(chain_name, use_helix)
 
 		if not failure_reason then
 			--Check that each command will be ready when called for
-			for step,element in pairs(skillchain_steps[chain_name]) do
+			for step,element in pairs(settings.skillchains[chain_name]) do
 
 				if abil_recasts[res.ability_recasts:with('en', 'Stratagems')['id']] < active_immanence+33*(5-step)+execution_time then
 					command_list[#command_list+1] = "Immanence"
@@ -307,11 +275,11 @@ function make_skillchain(chain_name, use_helix)
 				end
 
 				if step == 1 or not use_helix then
-					spell = spells_to_chain[element]['normal']
+					spell = settings.chain_spells[element]['normal']
 				else
-					spell = spells_to_chain[element]['helix']
-					if spell_recasts[res.spells:with('en', spell)['id']]/60 > execution_time and settings.allow_helix_recast_fallback then
-						spell = spells_to_chain[element]['normal']
+					spell = settings.chain_spells[element]['helix']
+					if spell_recasts[res.spells:with('en', spell)['id']]/60 > execution_time and settings.IO.allow_helix_recast_fallback then
+						spell = settings.chain_spells[element]['normal']
 					end
 				end
 				if spell_recasts[res.spells:with('en', spell)['id']]/60 < execution_time then
@@ -374,9 +342,9 @@ function run_commands(command_list, command_index, chain_name, start_time)
 				coroutine.sleep(settings.wait.post_ja)
 			else
 				if command_index <= 3 then --da-imma-OPENER or imma-OPENER-imma
-					windower.send_command("input /"..settings.announce_channel.." Opening "..chain_name..": "..current_command)
+					windower.send_command("input /"..settings.IO.announce_channel.." Opening "..chain_name..": "..current_command)
 				else
-					windower.send_command("input /"..settings.announce_channel.." "..chain_name..": "..current_command)
+					windower.send_command("input /"..settings.IO.announce_channel.." "..chain_name..": "..current_command)
 				end
 				if current_command:find('helix') then
 					if command_index <= 3 then
@@ -395,10 +363,10 @@ function run_commands(command_list, command_index, chain_name, start_time)
 end
 
 function feedback(message, priority)
-	if settings.verbosity >= priority then
-		if settings.feedback_channel == "console" then
+	if settings.IO.verbosity >= priority then
+		if settings.IO.feedback_channel == "console" then
 			print(message)
-		elseif settings.feedback_channel == "chat" then
+		elseif settings.IO.feedback_channel == "chat" then
 			windower.add_to_chat(207, message)
 		end
 	end
